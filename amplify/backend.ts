8f09479ad9533bb12c1d7b2cde2e90bbe84c8ca8
LB <lbutton@riacoding.net>
2025-09-01 16:31:51 -0700

    fix corrects ENV_NAME

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -180,8 +180,8 @@ backend.webhookProcessor.addEnvironment('SQS_QUEUE_URL', squareWebhook.squareQue
 backend.webhookProcessor.addEnvironment('SQS_DLQ_URL', squareWebhook.squareDLQURL.queueUrl)
 backend.webhookProcessor.addEnvironment('ORDERS_TABLE', ordersTable.tableName)
 backend.webhookProcessor.addEnvironment('IDEMPOTENCY_TABLE', squareWebhook.idempotencyTable.tableName)
+backend.webhookProcessor.addEnvironment('ENV_NAME', ENV_NAME)
 backend.squareAuth.addEnvironment('APP_BASE_URL', APP_BASE_URL)
-backend.squareAuth.addEnvironment('ENV_NAME', ENV_NAME)
 backend.twilioInbound.addEnvironment('APP_BASE_URL', APP_BASE_URL)
 
 squareWebhook.squareTopic.grantPublish(backend.webhook.resources.lambda)
