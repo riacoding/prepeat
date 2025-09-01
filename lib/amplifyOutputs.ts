cf753bf2e085347d56b22d64e7ceb2558dccf802
LB <lbutton@riacoding.net>
2025-09-01 09:19:42 -0700

    fix default outputs

--- lib/amplifyOutputs.ts
+++ lib/amplifyOutputs.ts
@@ -1,9 +1,11 @@
 // app/lib/amplifyOutputs.ts
-import dev from '../amplify_outputs.dev.json'
-import sandbox from '../amplify_outputs.json'
-import prod from '../amplify_outputs.prod.json'
+//import dev from '../amplify_outputs.dev.json'
+import standard from '../amplify_outputs.json'
+//import prod from '../amplify_outputs.prod.json'
 
 console.log('BACKEND', process.env.NEXT_PUBLIC_BACKEND)
 
-export const outputs =
-  process.env.NEXT_PUBLIC_BACKEND === 'dev' ? dev : process.env.NEXT_PUBLIC_BACKEND === 'prod' ? prod : sandbox
+// export const outputs =
+//   process.env.NEXT_PUBLIC_BACKEND === 'dev' ? dev : process.env.NEXT_PUBLIC_BACKEND === 'prod' ? prod : sandbox
+
+export const outputs = standard
