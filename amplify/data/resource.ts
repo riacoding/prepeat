a35a236cadcd37dfb47fa555286a6542ffe2884c
LB <lbutton@riacoding.net>
2025-10-15 08:52:54 -0700

    adds belongTo on inverse side

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -244,6 +244,7 @@ const schema = a
         merchantId: a.id().required(),
         modifierId: a.string().required(), // Square modifier id
         modifierListId: a.string().required(), // Square modifier list id
+        modifierList: a.belongsTo('ModifierList', ['merchantId', 'modifierListId']),
         name: a.string().required(),
         priceCents: a.integer().required(),
         currency: a.string().required(),
