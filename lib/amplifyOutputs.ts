81bc695c9f3d5037432cef483c225d13662f195d
LB <lbutton@riacoding.net>
2025-10-30 10:13:12 -0700

    reset to standard

--- lib/amplifyOutputs.ts
+++ lib/amplifyOutputs.ts
@@ -1,6 +1,6 @@
 // app/lib/amplifyOutputs.ts
-import dev from '../amplify_outputs.dev.json'
-//import standard from '../amplify_outputs.json'
+//import dev from '../amplify_outputs.dev.json'
+import standard from '../amplify_outputs.json'
 //import prod from '../amplify_outputs.prod.json'
 
 //console.log('BACKEND', process.env.NEXT_PUBLIC_BACKEND)
@@ -8,4 +8,4 @@ import dev from '../amplify_outputs.dev.json'
 // export const outputs =
 //   process.env.NEXT_PUBLIC_BACKEND === 'dev' ? dev : process.env.NEXT_PUBLIC_BACKEND === 'prod' ? prod : sandbox
 
-export const outputs = dev
+export const outputs = standard
